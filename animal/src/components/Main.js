6163141f79711dd882fa926f882b58499672faf1
root <root@STDLTUC-L069.localdomain>
2021-06-07 01:19:18 +0300

    create app

---
+++ animal/src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HornedBeasts from './HornedBeasts.js';
+
+class Main extends React.Component {
+
+    render() {
+       
+        return (
+            <main>
+             <HornedBeasts />
+
+            </main>
+        )
+    }
+}
+export default Main;
